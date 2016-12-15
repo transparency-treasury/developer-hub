a543497ba49269ae142c7018db281443f20485d6
unknown <J1JEA01@J1JEA01L0.rb.win.frb.org>
2016-12-15 11:59:58 -0600

    fixing branch

---
+++ _site/static/js/apiDocumentation/get.js
@@ -0,0 +1,17 @@
+'use strict'
+
+ramlCode.methods.getRaml = function(url, callback){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function(){
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+       		// Action to be performed when the document is read;
+			if (callback){
+				callback(xmlhttp.responseText);
+			}
+
+			return xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
